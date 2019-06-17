65e4dcc2db4a888bbaf85a6eee3c40b69364e71f
somethingx02 <zhulixinga@126.com>
2019-06-17 23:03:18 +0800

    20190617 5th commit

--- TopicCoherenceComputeJavaProj/TCCalculateTFileProj/TCCalculateTFileCore/mainpkg/Main.java
+++ TopicCoherenceComputeJavaProj/TCCalculateTFileProj/TCCalculateTFileCore/mainpkg/Main.java
@@ -11,14 +11,14 @@ public class Main {
 		// specify the wikipedia_bd (downloaded from the website)
 		TCoherenceCalculate tCoherenceCalculate=new TCoherenceCalculate("/data/zlx/java_workspaces/wikipedia_bd","C_V");
 
-		double tcavg=tCoherenceCalculate.TCoherenceBleiLDAFileInput("/data1/zlx2/Baselines_JTW/ste/steTopicMatrix100words.txt");
-		System.out.println(tcavg);
-		tcavg=tCoherenceCalculate.TCoherenceBleiLDAFileInput("/data1/zlx2/Baselines_JTW/ste/steTopicMatrix150words.txt");
-		System.out.println(tcavg);
-		tcavg=tCoherenceCalculate.TCoherenceBleiLDAFileInput("/data1/zlx2/Baselines_JTW/ste/steTopicMatrix200words.txt");
-		System.out.println(tcavg);
+		//double tcavg=tCoherenceCalculate.TCoherenceBleiLDAFileInput("/data1/zlx2/Baselines_JTW/ste/steTopicMatrix100words.txt");
+		//System.out.println(tcavg);
+		//tcavg=tCoherenceCalculate.TCoherenceBleiLDAFileInput("/data1/zlx2/Baselines_JTW/ste/steTopicMatrix150words.txt");
+		//System.out.println(tcavg);
+		//tcavg=tCoherenceCalculate.TCoherenceBleiLDAFileInput("/data1/zlx2/Baselines_JTW/ste/steTopicMatrix200words.txt");
+		//System.out.println(tcavg);
 
-	    tcavg=tCoherenceCalculate.TCoherenceBleiLDAFileInput("/data1/zlx2/topical_wordvec_models_mirror3/save/11/jtwTopicMatrix.txt");
+	    double tcavg=tCoherenceCalculate.TCoherenceBleiLDAFileInput("/data1/zlx2/topical_wordvec_models_mirror3/save/11/jtwTopicMatrix.txt");
 		System.out.println(tcavg);
         
 	}
